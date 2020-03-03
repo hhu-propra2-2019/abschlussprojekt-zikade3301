de71d0109ab5e25723b2b00978ef758288e59719
MichaMotzi207 <michael.motzko@gmx.net>
2020-03-03 16:17:26 +0100

    included keycloack and an index-file + controller

---
+++ src/main/java/mops/module/indexController.java
@@ -0,0 +1,14 @@
+package mops.module;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class indexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
