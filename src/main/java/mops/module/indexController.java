1b270d2d0747db75dec91e3012a57aadd0173140
MichaMotzi207 <michael.motzko@gmx.net>
2020-03-03 16:41:24 +0100

    refactored the files as the slides told us to do - ModuleApplication is part of package "mops" - changed the Controller-Path to ("/module")

--- src/main/java/mops/module/indexController.java
+++ src/main/java/mops/module/indexController.java
@@ -1,10 +1,11 @@
 package mops.module;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/module")
 public class indexController {
 
     @GetMapping("/")
