f930c3a67c1e28444a688d4cedc51161e7249a85
MichaMotzi207 <michael.motzko@gmx.net>
2020-03-03 13:40:02 +0100

    created the project

---
+++ src/main/java/mops/module/ModuleApplication.java
@@ -0,0 +1,13 @@
+package mops.module;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModuleApplication.class, args);
+    }
+
+}
