724c5def3168629efe13f6de218c8883b7b32643
Zhorrid <xintake@gmx.de>
2020-03-04 14:00:52 +0100

    REFACTOR: 4 Spaces per Tab in checkstyle.xml

--- src/main/java/mops/module/indexController.java
+++ src/main/java/mops/module/indexController.java
@@ -6,8 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class indexController {
 
-  @GetMapping("/")
-  public String index() {
-    return "index";
-  }
+    @GetMapping("/")
+    public String index() {
+        return "index";
+
+    }
 }
