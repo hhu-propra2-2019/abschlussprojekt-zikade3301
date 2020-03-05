9810b04a9d177a3955428f32b918cfae485aa379
MichaMotzi207 <michael.motzko@gmx.net>
2020-03-05 14:02:07 +0100

    created a file for our IndexController testing;

---
+++ src/test/java/mops/module/IndexControllerTest.java
@@ -0,0 +1,15 @@
+package mops.module;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.junit.jupiter.api.Test;
+
+@WebMvcTest
+public class IndexControllerTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+
+
+
+}
