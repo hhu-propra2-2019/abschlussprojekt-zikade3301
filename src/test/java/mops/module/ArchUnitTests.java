049a856215c7edc572335a6b27b0dec11bef4c5b
Zhorrid <xintake@gmx.de>
2020-03-05 16:42:39 +0100

    REFACTOR: ArchUnitTests fixed

--- src/test/java/mops/module/ArchUnitTests.java
+++ src/test/java/mops/module/ArchUnitTests.java
@@ -3,45 +3,41 @@ package mops.module;
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
 
 
-import com.tngtech.archunit.core.domain.JavaAnnotation;
 import com.tngtech.archunit.core.domain.JavaClass;
-import com.tngtech.archunit.core.domain.JavaClasses;
-import com.tngtech.archunit.core.importer.ClassFileImporter;
 import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.ArchCondition;
 import com.tngtech.archunit.lang.ArchRule;
 import com.tngtech.archunit.lang.ConditionEvents;
-import org.aspectj.lang.annotation.Before;
-import org.junit.jupiter.api.BeforeAll;
+import com.tngtech.archunit.lang.SimpleConditionEvent;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.lang.annotation.Annotation;
-import java.util.Optional;
 
 @AnalyzeClasses(packages = "mops.module")
 public class ArchUnitTests {
 
+    @ArchTest
+    static final ArchRule controllerIsAnnotatedWithRequestMapping =
+            classes().that().haveNameMatching(".*Controller")
+                    .should().beAnnotatedWith(RequestMapping.class).andShould(new rule("/module", "Request-Mapping incorrect!"));
+
     public static class rule extends ArchCondition<JavaClass> {
 
-        public rule(String description, Object... args) {
+        String correctValue;
+
+        public rule(String correctValue, String description, Object... args) {
             super(description, args);
+            this.correctValue = correctValue;
         }
 
         @Override
         public void check(JavaClass item, ConditionEvents events) {
-            Optional<JavaAnnotation<JavaClass>>
-                    annotation = item.getAnnotations().stream().filter(javaAnn -> javaAnn.getClass().equals(RequestMapping.class))
-            .findAny();
+            // This will never be null, as we check for an annotation of type RequestMapping beforehand.
+            RequestMapping annotation = item.getAnnotationOfType(RequestMapping.class);
 
-            annotation.ifPresent(a->a.get("value"));
+            if (annotation.value().length == 0 || !annotation.value()[0].equals(correctValue)) {
+                events.add(SimpleConditionEvent.violated(item, "Request-Mapping annotation doesn't contain the required parameter."));
+            }
         }
     }
-
-
-    @ArchTest
-    static final ArchRule controllerIsAnnotaedWithRequestMapping =
-            classes().that().haveNameMatching(".*Controller")
-                    .should().beAnnotatedWith(RequestMapping.class).andShould()
 
 }
