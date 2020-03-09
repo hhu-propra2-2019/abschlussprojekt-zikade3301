eaa9216c8f1c918f6dea13fbfb3f6f98624a47b5
therealhouri <therealhouri@gmail.com>
2020-03-09 13:20:46 +0100

    REFACTOR: removed repositories & services

--- src/main/java/mops/module/repositories/AntragRepository.java
+++
@@ -1,7 +0,0 @@
-package mops.module.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface AntragRepository extends CrudRepository<Antrag, Long> {
-    void updateModul(Antrag antrag);
-}
--- src/main/java/mops/module/repositories/ModulRepository.java
+++
@@ -1,8 +0,0 @@
-package mops.module.repositories;
-
-import mops.module.database.Modul;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ModulRepository extends CrudRepository<Modul, Long> {
-    Antrag getCorrespondingAntrag(Modul modul);
-}
