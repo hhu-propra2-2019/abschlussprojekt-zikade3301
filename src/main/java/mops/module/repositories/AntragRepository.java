adc6f426ae55d778e8f44cc27b3f1de22db96d5c
therealhouri <therealhouri@gmail.com>
2020-03-09 14:07:20 +0100

    ADD: Repositories

---
+++ src/main/java/mops/module/repositories/AntragRepository.java
@@ -0,0 +1,6 @@
+package mops.module.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface AntragRepository extends CrudRepository<Antrag, Long> {
+}
---
+++ src/main/java/mops/module/repositories/ModulRepository.java
@@ -0,0 +1,7 @@
+package mops.module.repositories;
+
+import mops.module.database.Modul;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ModulRepository extends CrudRepository<Modul, Long> {
+}
