1dafdb32d15be7dc1fe3603cabc4b33f7be98b0d
Iven Buchholz <Ivenbu@googlemail.com>
2020-03-10 15:26:01 +0100

    ADD: ModuleController with simple GetMapping

---
+++ src/main/java/mops/module/ModuleController.java
@@ -0,0 +1,33 @@
+package mops.module;
+
+
+import org.keycloak.KeycloakPrincipal;
+import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/module")
+public class ModuleController {
+
+    private Account createAccountFromPrincipal(KeycloakAuthenticationToken token) {
+        KeycloakPrincipal principal = (KeycloakPrincipal) token.getPrincipal();
+        return new Account(
+                principal.getName(),
+                principal.getKeycloakSecurityContext().getIdToken().getEmail(),
+                null,
+                token.getAccount().getRoles());
+    }
+
+
+
+    @GetMapping("/meineModule")
+    @Secured("ROLE_orga")
+    public String module(KeycloakAuthenticationToken token, Model model) {
+        model.addAttribute("account", createAccountFromPrincipal(token));
+        return "module";
+    }
+}
