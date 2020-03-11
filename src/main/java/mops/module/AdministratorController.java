93b991ca66c79f85ce3d311c2eabb92ce5010929
Iven Buchholz <Ivenbu@googlemail.com>
2020-03-11 14:55:53 +0100

    REFACTOR: AdministratorController - new GetMapping

--- src/main/java/mops/module/AdministratorController.java
+++ src/main/java/mops/module/AdministratorController.java
@@ -1,11 +1,26 @@
 package mops.module;
 
+import static mops.module.keycloak.KeycloakMopsAccount.createAccountFromPrincipal;
+
+import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.annotation.SessionScope;
 
 
 @Controller
+@SessionScope
 @RequestMapping("/module")
 public class AdministratorController {
+
+    @GetMapping("/administrator")
+    @Secured("ROLE_orga")
+    public String administrator(KeycloakAuthenticationToken token, Model model) {
+        model.addAttribute("account", createAccountFromPrincipal(token));
+        return "administrator";
+    }
 
 }
