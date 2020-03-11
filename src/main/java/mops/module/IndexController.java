4d8ee707b2773c522fcc50fce8db8cb6b1c3176a
MichaMotzi207 <michael.motzko@gmx.net>
2020-03-11 13:27:04 +0100

    ADDED: Test SessionScope Annotation for existing Controller

--- src/main/java/mops/module/IndexController.java
+++ src/main/java/mops/module/IndexController.java
@@ -6,9 +6,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.annotation.SessionScope;
 
 
 @Controller
+@SessionScope
 @RequestMapping("/module")
 public class IndexController {
 
