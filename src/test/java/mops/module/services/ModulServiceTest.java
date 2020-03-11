47540a53f2ddea505a1f5da5d1d33215aaabd338
therealhouri <therealhouri@gmail.com>
2020-03-11 17:53:30 +0100

    REFACTOR: addModulTests

--- src/test/java/mops/module/services/ModulServiceTest.java
+++ src/test/java/mops/module/services/ModulServiceTest.java
@@ -2,26 +2,34 @@ package mops.module.services;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import mops.module.database.Antrag;
 import mops.module.database.Modul;
 import mops.module.database.Modulkategorie;
 import mops.module.database.Veranstaltung;
 import mops.module.repositories.AntragsRepository;
 import mops.module.repositories.ModulSnapshotRepository;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class ModulServiceTest {
-    private static ModulService modulService;
-    private static JSONService jsonService;
+    private ModulService modulService;
+    private JSONService jsonService;
+    private AntragsRepository antragsRepository;
+    private ModulSnapshotRepository modulSnapshotRepository;
 
-    @BeforeAll
-    static void init() {
-        AntragsRepository antragsRepository = mock(AntragsRepository.class);
-        ModulSnapshotRepository modulSnapshotRepository = mock(ModulSnapshotRepository.class);
+
+    @BeforeEach
+    public void init() {
+        antragsRepository = mock(AntragsRepository.class);
+        modulSnapshotRepository = mock(ModulSnapshotRepository.class);
         jsonService = new JSONService();
         modulService = new ModulService(antragsRepository, modulSnapshotRepository, jsonService);
     }
@@ -86,5 +94,59 @@ public class ModulServiceTest {
         Modul diffs = modulService.calculateModulDiffs(modul1, modul2);
         assertThat(diffs.getModulkategorie()).isEqualTo(Modulkategorie.BACHELORARBEIT);
         assertThat(diffs.getVeranstaltungen()).isEqualTo(null);
+    }
+
+    @Test
+    public void addModulTestNewModul() {
+        Modul newModul = new Modul();
+        newModul.setId(1L);
+        newModul.setModulkategorie(Modulkategorie.MASTERARBEIT);
+        Veranstaltung veranstaltung1 = new Veranstaltung();
+        Veranstaltung veranstaltung2 = new Veranstaltung();
+        veranstaltung1.setId(1L);
+        veranstaltung2.setId(2L);
+        List<Veranstaltung> veranstaltungen1 = new ArrayList<>();
+        veranstaltungen1.add(veranstaltung2);
+        veranstaltung1.setVoraussetzungenTeilnahme(veranstaltungen1);
+        newModul.setVeranstaltungen(veranstaltungen1);
+        LocalDateTime approveDate = LocalDateTime.now();
+
+        Optional<Modul> empty = Optional.empty();
+        when(modulSnapshotRepository.findById(1L)).thenReturn(empty);
+        modulService.addModul(newModul, approveDate);
+
+        Antrag newAntrag = modulService.toAntrag(newModul, approveDate);
+
+        System.out.println("*****************");
+        System.out.println(antragsRepository.count());
+        antragsRepository.save(newAntrag);
+        System.out.println(antragsRepository.count());
+
+    }
+
+    @Disabled
+    @Test
+    public void addModulTestoverwrite() {
+        Modul oldModul = new Modul();
+        oldModul.setId(1L);
+        oldModul.setModulkategorie(Modulkategorie.MASTERARBEIT);
+        Veranstaltung veranstaltung1 = new Veranstaltung();
+        veranstaltung1.setId(1L);
+        List<Veranstaltung> veranstaltungen1 = new ArrayList<Veranstaltung>();
+        veranstaltungen1.add(veranstaltung1);
+        veranstaltung1.setVoraussetzungenTeilnahme(veranstaltungen1);
+        oldModul.setVeranstaltungen(veranstaltungen1);
+
+        Modul newModul = new Modul();
+        newModul.setId(1L);
+        newModul.setModulkategorie(Modulkategorie.BACHELORARBEIT);
+        Veranstaltung veranstaltung = new Veranstaltung();
+        veranstaltung.setId(1L);
+        List<Veranstaltung> veranstaltungen = new ArrayList<Veranstaltung>();
+        veranstaltungen.add(veranstaltung);
+        veranstaltung.setVoraussetzungenTeilnahme(veranstaltungen);
+        newModul.setVeranstaltungen(veranstaltungen);
+
+        //when(modulSnapshotRepository.findById(newModul.getId())).thenReturn(newModul);
     }
 }
