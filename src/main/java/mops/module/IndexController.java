1efe98f5d631de528043c1ee1d635eb4fb8f2d8a
MichaMotzi207 <michael.motzko@gmx.net>
2020-03-13 13:42:29 +0100

    ADD: basic searchfunction with SQL-Query

--- src/main/java/mops/module/IndexController.java
+++ src/main/java/mops/module/IndexController.java
@@ -10,6 +10,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.context.annotation.SessionScope;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 
 @Controller
 @SessionScope
@@ -34,9 +39,45 @@ public class IndexController {
 
     @GetMapping("/search")
     public String searchMethodTmp(@RequestParam String searchField) {
-        // Hier Suche implementieren
-        System.out.println(searchField);
+        searchForModule(searchField);
         return "index";
     }
 
+
+    // "Tabelle" durch Tabellennamen ersetzen
+    public void searchForModule(String searchinput) {
+        try {
+            String url = "jdbc:postgresql://localhost:3301/Modulhandbuch";
+            Connection conn = DriverManager.getConnection(url,"root","zikade3301");
+            Statement stmt = conn.createStatement();
+            ResultSet searchResult;
+
+            // ZUR NOT NEUE TABLE ZU IMPLEMENTIERUNGSZWECKEN
+//            String sql1 = "CREATE TABLE Tabelle " +
+//                    "(id INTEGER not NULL, " +
+//                    " first VARCHAR(255), " +
+//                    " PRIMARY KEY ( id ))";
+//            stmt.executeUpdate(sql1);
+//            String sql2 = "INSERT INTO Tabelle (id, first) VALUES (1, 'MICHA')";
+//            stmt.executeUpdate(sql2);
+//            String sql3 = "INSERT INTO Tabelle (id, first) VALUES (2, 'Roman')";
+//            stmt.executeUpdate(sql3);
+//            String sql4 = "INSERT INTO Tabelle (id, first) VALUES (3, 'Ein ICE fährt schnell')";
+//            stmt.executeUpdate(sql4);
+//            String sql5 = "INSERT INTO Tabelle (id, first) VALUES (4, 'IVEN')";
+//            stmt.executeUpdate(sql5);
+
+            // TODO KEIN SQL INJECTION
+            searchResult = stmt.executeQuery("select * From Tabelle WHERE first like '%"+searchinput+"%'");
+
+            while ( searchResult.next() ) {
+                String inhalt = searchResult.getString(2);
+                System.out.println(inhalt + " enthält " + searchinput);
+            }
+            conn.close();
+        } catch (Exception e) {
+            System.err.println("Got an exception! ");
+            System.err.println(e.getMessage());
+        }
+    }
 }
