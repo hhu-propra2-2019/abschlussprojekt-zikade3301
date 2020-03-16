f79f956678ed8b648eca407bd9d9d415a43346a2
MichaMotzi207 <michael.motzko@gmx.net>
2020-03-16 16:32:06 +0100

    ADD: Controllermethod for new Modules. Needs to be finished in cooperation with the DB Team

--- src/main/java/mops/module/ModulbeauftragterController.java
+++ src/main/java/mops/module/ModulbeauftragterController.java
@@ -2,12 +2,24 @@ package mops.module;
 
 import static mops.module.keycloak.KeycloakMopsAccount.createAccountFromPrincipal;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import javax.persistence.FetchType;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import mops.module.database.Modul;
+import mops.module.database.Modulbeauftragter;
+import mops.module.database.Modulkategorie;
+import mops.module.database.Veranstaltung;
 import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.context.annotation.SessionScope;
 
 
@@ -22,4 +34,32 @@ public class ModulbeauftragterController {
         model.addAttribute("account", createAccountFromPrincipal(token));
         return "modulbeauftragter";
     }
+
+    // TODO Erstellung in Kooperation mit DB-Team
+    @RequestMapping(path = "/", method = RequestMethod.POST)
+    public String result(
+            @RequestParam(name = "title", required = true)
+                    String titelDeutsch,
+            @RequestParam(name = "titleEnglish", required = true)
+                    String titelEnglisch,
+            //            @RequestParam(name = "lessons", required = true)
+            //                    String veranstaltungen,
+            @RequestParam(name = "organizer", required = true)
+                    String modulbeauftragter,
+            @RequestParam(name = "cp", required = true)
+                    String creditPoints,
+            //            @RequestParam(name = "subject", required = true)
+            //                    String studiengang,
+            @RequestParam(name = "modulcategory", required = true)
+                    String modulkategorie,
+            //          sichtbarkeit
+            //          timeStamp
+            //          timeStamp
+            Model model) {
+        return "modulbeauftragter";
+    }
+
+
+
+
 }
