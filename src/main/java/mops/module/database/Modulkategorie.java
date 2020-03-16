1cd452455d5841a2fd5e7e923e1a45d8239dcb66
Zhorrid <xintake@gmx.de>
2020-03-16 15:39:52 +0100

    REFACTOR: Services and database refactored

--- src/main/java/mops/module/database/Modulkategorie.java
+++ src/main/java/mops/module/database/Modulkategorie.java
@@ -3,7 +3,7 @@ package mops.module.database;
 public enum Modulkategorie {
     PFLICHT_INFO(1), PFLICHT_MATHE(2), NEBENFACH(3), PRAXIS(4),
     WAHLPFLICHT_BA(5), WAHLPFLICHT_MA(6), BACHELORARBEIT(7),
-    PROJEKTARBEIT(8), MASTERARBEIT(9);
+    PROJEKTARBEIT(8), MASTERARBEIT(9), SONSTIGE(10);
 
     private final int value;
 
