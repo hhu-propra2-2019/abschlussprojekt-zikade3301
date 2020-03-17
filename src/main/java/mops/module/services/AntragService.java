d7ab8bd263ef774b2483c6c76d002c0771f39526
MichaMotzi207 <56838501+MichaMotzi207@users.noreply.github.com>
2020-03-17 15:42:52 +0100

    Update AntragService.java

--- src/main/java/mops/module/services/AntragService.java
+++ src/main/java/mops/module/services/AntragService.java
@@ -20,7 +20,7 @@ public class AntragService {
     private final ModulSnapshotRepository modulSnapshotRepository;
 
     /**
-     * Erstellt einen Eintrag aus einem Modul.
+     * Erstellt einen Änderungsantrag aus einem Modul heraus
      *
      * @param modul Modul auf welches der Antrag angewendet wird
      * @return
