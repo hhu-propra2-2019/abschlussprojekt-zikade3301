82505de22a5f57cc908e516c1f0740fef0157990
Zhorrid <xintake@gmx.de>
2020-03-18 16:59:13 +0100

    ADD: First GraphQLDataFetcher

---
+++ src/main/java/mops/module/api/GraphQLDataFetchers.java
@@ -0,0 +1,27 @@
+package mops.module.api;
+
+import graphql.schema.DataFetcher;
+import mops.module.repositories.AntragRepository;
+import mops.module.repositories.ModulSnapshotRepository;
+import mops.module.services.AntragService;
+import mops.module.services.ModulService;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GraphQLDataFetchers {
+
+    private ModulService modulService;
+    private AntragService antragService;
+
+    public GraphQLDataFetchers(AntragRepository antragRepository, ModulSnapshotRepository modulSnapshotRepository) {
+        modulService = new ModulService(antragRepository, modulSnapshotRepository);
+        antragService = new AntragService(antragRepository, modulSnapshotRepository);
+    }
+
+    public DataFetcher getModulByIdDataFetcher() {
+        return dataFetchingEnvironment -> {
+            String modulId = dataFetchingEnvironment.getArgument("id");
+            return modulService.getModulById(Long.parseLong(modulId));
+        };
+    }
+}
