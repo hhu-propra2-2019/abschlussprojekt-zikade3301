86e364281a2d0f89eb14d72f46512a8565e8a8af
MichaMotzi207 <michael.motzko@gmx.net>
2020-03-19 16:13:43 +0100

    REFACTOR: modulcategory is getting translates now
    Co-authored-by: EliasK93 <elias.kempkens@hhu.de>

--- src/main/java/mops/module/database/Modulkategorie.java
+++ src/main/java/mops/module/database/Modulkategorie.java
@@ -4,4 +4,26 @@ public enum Modulkategorie {
     PFLICHT_INFO, PFLICHT_MATHE, NEBENFACH, PRAXIS,
     WAHLPFLICHT_BA, WAHLPFLICHT_MA, BACHELORARBEIT,
     PROJEKTARBEIT, MASTERARBEIT, SONSTIGE;
+
+
+    /**
+     * Translates the DB-category.
+     *
+     * @return The German version of the category.
+     */
+    public String translate() {
+
+        switch (this) {
+            case PFLICHT_INFO: return "Informatik Pflichtmodul";
+            case PFLICHT_MATHE: return "Mathematik Pflichtmodul";
+            case NEBENFACH: return "Nebenfach";
+            case PRAXIS: return "Praxis- und Berufsorientierung";
+            case WAHLPFLICHT_BA: return "Wahlpflichtbereich (B.Sc.)";
+            case WAHLPFLICHT_MA: return "Wahlpflichtbereich (M.Sc.)";
+            case BACHELORARBEIT: return "Bachelorarbeit";
+            case PROJEKTARBEIT: return "Projektarbeit";
+            case MASTERARBEIT: return "Masterarbeit";
+            default: return "Sonstige";
+        }
+    }
 }
