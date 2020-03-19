9769b8acb8d82404820f7eee1fe0b0eb5d979932
therealhouri <therealhouri@gmail.com>
2020-03-19 15:19:58 +0100

    ADD: PdfService class

---
+++ src/main/java/mops/module/services/PdfService.java
@@ -0,0 +1,10 @@
+package mops.module.services;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+
+public class PdfService {
+
+    public static PDDocument generatePdf(){
+        return new PDDocument();
+    }
+}
