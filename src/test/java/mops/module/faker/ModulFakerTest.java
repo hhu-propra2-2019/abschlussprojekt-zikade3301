d321e4cc0831bbb2fcbb4b72798df62aa8faed73
therealhouri <therealhouri@gmail.com>
2020-03-19 14:52:53 +0100

    REFACTOR: try without activeprofile

--- src/test/java/mops/module/faker/ModulFakerTest.java
+++ src/test/java/mops/module/faker/ModulFakerTest.java
@@ -27,7 +27,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("dev")
 public class ModulFakerTest {
     private ModulService modulService;
     private AntragService antragService;
