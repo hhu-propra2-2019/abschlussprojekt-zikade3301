2824eb933bfd979571c5c43fb130fdc1af4a4b29
abot64 <ann.lenden@hhu.de>
2020-03-20 18:13:00 +0100

    REFACTOR: Fulltextsearch on Veranstaltungsinhalte now uses JPA instead of JDBC

--- src/main/java/mops/module/services/SuchService.java
+++ src/main/java/mops/module/services/SuchService.java
@@ -1,10 +1,5 @@
 package mops.module.services;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 import mops.module.database.Modul;
 import mops.module.repositories.ModulSnapshotRepository;
@@ -30,27 +25,9 @@ public class SuchService {
      * Uses Postgres internal FullTextSearch to get results in module description
      *
      * @param searchinput given searchterm from user
-     * @param conn        connection to database
      * @return list of modules where searchterm was found in description
      */
-    public List<Modul> searchInVeranstaltungsbeschreibung(String searchinput, Connection conn) {
-        List<Modul> results = new ArrayList<>();
-        try {
-            PreparedStatement stmt = conn.prepareStatement(
-                    "SELECT modul_id FROM veranstaltung WHERE to_tsquery('german', inhalte) @@ to_tsvector('german', ?)"
-            );
-            stmt.setString(1, searchinput);
-
-            ResultSet searchResult = stmt.executeQuery();
-
-            while (searchResult.next()) {
-                Long id = searchResult.getLong("modul_id");
-                Modul modul = modulSnapshotRepository.findById(id).orElse(null);
-                results.add(modul);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return results;
+    public List<Modul> searchInVeranstaltungsbeschreibung(String searchinput) {
+        return modulSnapshotRepository.fullTextSearchForModule(searchinput);
     }
 }
