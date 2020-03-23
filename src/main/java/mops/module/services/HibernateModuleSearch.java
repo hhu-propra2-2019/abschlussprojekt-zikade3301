5c902c7a12fec90916ef2e828802d505263a3626
abot64 <ann.lenden@hhu.de>
2020-03-23 14:17:36 +0100

    ADD: HibernateSearch - this time with correct java class included

---
+++ src/main/java/mops/module/services/HibernateModuleSearch.java
@@ -0,0 +1,49 @@
+package mops.module.services;
+
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import mops.module.database.Modul;
+import org.hibernate.search.jpa.FullTextEntityManager;
+import org.hibernate.search.jpa.Search;
+import org.hibernate.search.query.dsl.QueryBuilder;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+public class HibernateModuleSearch {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public void initIndex() throws InterruptedException {
+        FullTextEntityManager fullTextEntityManager = Search.getFullTextEntityManager(entityManager);
+        fullTextEntityManager.createIndexer().startAndWait();
+    }
+
+    @Transactional
+    public List<Modul> search(String searchinput) {
+        FullTextEntityManager fullTextEntityManager = Search.getFullTextEntityManager(entityManager);
+
+        QueryBuilder queryBuilder = fullTextEntityManager.getSearchFactory().buildQueryBuilder().forEntity(Modul.class).get();
+
+        //TODO add all fields and boost individual fields in result (eg name)
+        org.apache.lucene.search.Query query = queryBuilder
+                .simpleQueryString()
+                .onField("titelDeutsch").boostedTo(10f)
+                .andField("studiengang").boostedTo(3f)
+                .andField("veranstaltung.titel").boostedTo(10f)
+                .andField("veranstaltung.inhalte").boostedTo(5f)
+                .andField("veranstaltung.lernergebnisse")
+                .andField("veranstaltung.sprache")
+                .matching(searchinput)
+                .createQuery();
+
+        org.hibernate.search.jpa.FullTextQuery jpaQuery = fullTextEntityManager.createFullTextQuery(query, Modul.class);
+
+        List<Modul> results = jpaQuery.getResultList();
+
+        return results;
+    }
+
+}
