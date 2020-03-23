ee722c0e4a67a758b756b4c97aa248ccd3ddf728
Zhorrid <xintake@gmx.de>
2020-03-23 16:43:35 +0100

    ADD: Controller for PDF download
    Co-authored-by: therealhouri <therealhouri@gmail.com>

---
+++ src/main/java/mops/module/controller/PdfDownloadController.java
@@ -0,0 +1,44 @@
+package mops.module.controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import mops.module.database.Modul;
+import mops.module.generator.ModulFaker;
+import mops.module.services.PdfService;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/module")
+public class PdfDownloadController {
+
+    /**
+     *
+     * @param response
+     */
+    @GetMapping("/pdf")
+    public void getPdf(HttpServletResponse response) {
+
+        List<Modul> module = new ArrayList<>();
+        for(int i = 0; i < 5; i++) {
+            module.add(ModulFaker.generateFakeModul());
+        }
+        PDDocument document1 = PdfService.generatePdf(module);
+
+        response.setContentType("application/pdf");
+        response.addHeader("Content-Disposition", "attachment; filename=Modulhandbuch.pdf");
+
+        try {
+            document1.save(response.getOutputStream());
+            response.flushBuffer();
+            document1.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
