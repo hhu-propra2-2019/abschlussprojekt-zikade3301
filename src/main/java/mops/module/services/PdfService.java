67e676a323a9afb68d493d1eb23f025a3c928ca8
Zhorrid <xintake@gmx.de>
2020-03-23 16:44:12 +0100

    REFACTOR: Markdown problems solved
    Co-authored-by: therealhouri <therealhouri@gmail.com>

--- src/main/java/mops/module/services/PdfService.java
+++ src/main/java/mops/module/services/PdfService.java
@@ -1,6 +1,10 @@
 package mops.module.services;
 
 import com.qkyrie.markdown2pdf.Markdown2PdfConverter;
+import com.qkyrie.markdown2pdf.internal.exceptions.ConversionException;
+import com.qkyrie.markdown2pdf.internal.exceptions.Markdown2PdfLogicException;
+import com.qkyrie.markdown2pdf.internal.writing.SimpleFileMarkdown2PdfWriter;
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
@@ -19,69 +23,66 @@ public class PdfService {
         for (Modul modul : module) {
             appendPdf(document, generatePdf(modul));
         }
-        try {
-            document.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
         return document;
     }
 
     public static PDDocument generatePdf(Modul modul) {
-        PDDocument document = new PDDocument();
         String str = buildString(modul);
-        markdown2PdfConverter.readFrom(() -> str).writeTo(bytes -> {
-            try {
-                document.load(bytes);
-                document.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
+        final PDDocument[] document = new PDDocument[1];
+        try {
+            markdown2PdfConverter.readFrom(() -> str).writeTo(bytes -> {
+                try {
+                    document[0] = PDDocument.load(bytes);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }).doIt();
+        } catch (ConversionException | Markdown2PdfLogicException e) {
+            e.printStackTrace();
+        }
 
-        return document;
-
+        return document[0];
     }
 
     private static String buildString(Modul modul) {
         StringBuilder str = new StringBuilder();
-        str.append("#" + modul.getTitelDeutsch() + "\\");
-        str.append("#" + modul.getTitelEnglisch() + "\\");
-        str.append("## Studiengang\\");
-        str.append(modul.getStudiengang() + "\\");
-        str.append("## Leistungspunkte\\");
-        str.append(modul.getGesamtLeistungspunkte() + "\\");
-        str.append("## Lehrveranstaltungen\\");
+        str.append("#" + modul.getTitelDeutsch() + "\n");
+        str.append("#" + modul.getTitelEnglisch() + "\n");
+        str.append("## Studiengang\n");
+        str.append(modul.getStudiengang() + "\n");
+        str.append("## Leistungspunkte\n");
+        str.append(modul.getGesamtLeistungspunkte() + "\n");
+        str.append("## Lehrveranstaltungen\n");
         modul.getVeranstaltungen().stream()
                 .map(Veranstaltung::getTitel)
-                .map(x -> x + "\\")
+                .map(x -> x + "\n")
                 .forEach(str::append);
-        str.append("## Inhalte\\");
+        str.append("## Inhalte\n");
         modul.getVeranstaltungen().stream()
                 .map(Veranstaltung::getBeschreibung)
                 .map(Veranstaltungsbeschreibung::getInhalte)
-                .map(x -> x + "\\")
+                .map(x -> x + "\n")
                 .forEach(str::append);
-        str.append("## Literatur\\");
+        str.append("## Literatur\n");
         modul.getVeranstaltungen().stream()
                 .map(Veranstaltung::getBeschreibung)
                 .map(Veranstaltungsbeschreibung::getLiteratur)
-                .map(x -> x + "\\")
+                .map(x -> x + "\n")
                 .forEach(str::append);
-        str.append("## Verwendbarkeit\\");
+        str.append("## Verwendbarkeit\n");
         modul.getVeranstaltungen().stream()
                 .map(Veranstaltung::getBeschreibung)
                 .map(Veranstaltungsbeschreibung::getVerwendbarkeit)
-                .map(x -> x + "\\")
+                .map(x -> x + "\n")
                 .forEach(str::append);
-        str.append("## Teilnahmevoraussetzungen\\");
+        str.append("## Teilnahmevoraussetzungen\n");
         modul.getVeranstaltungen().stream()
                 .map(Veranstaltung::getVoraussetzungenTeilnahme)
-                .map(x -> x + "\\")
+                .map(x -> x + "\n")
                 .forEach(str::append);
-        str.append("## Studiengang\\");
+        str.append("## Studiengang\n");
         modul.getModulbeauftragte().stream()
-                .map(x -> x + "\\")
+                .map(x -> x + "\n")
                 .forEach(str::append);
 
         return str.toString();
