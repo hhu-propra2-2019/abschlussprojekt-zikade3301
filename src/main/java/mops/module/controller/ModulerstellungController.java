577366d40c3783857b16e0f110546a642905de9f
EliasK93 <elias.kempkens@hhu.de>
2020-03-23 11:36:38 +0100

    REFACTOR: worked on some details in the PostMapping-Controller & the html form
    Co-authored-by: MichaMotzi207 <michael.motzko@gmx.net>

--- src/main/java/mops/module/controller/ModulerstellungController.java
+++ src/main/java/mops/module/controller/ModulerstellungController.java
@@ -57,7 +57,7 @@ public class ModulerstellungController {
     //    TODO TEST HIERFÜR
     @PostMapping("/modulerstellung")
     @Secured("ROLE_orga")
-    public String addCreationAntrag(@RequestBody Map<String,String> allParams,
+    public String addCreationAntrag(@RequestParam Map<String,String> allParams,
                                     Model model,
                                     KeycloakAuthenticationToken token) {
 
@@ -69,7 +69,7 @@ public class ModulerstellungController {
         String modulbeauftragte = allParams.get("modulbeauftragte");
         Modulkategorie modulkategorie = Modulkategorie.valueOf(allParams.get("modulkategorie"));
 
-        //Separieren der Formulardaten für einzelne Veranstaltungen
+        //Einlesen der Formulardaten für einzelne Veranstaltungen
         int veranstaltungsanzahl = Integer.parseInt(allParams.get("veranstaltungsanzahl"));
         Set<Veranstaltung> veranstaltungen = new HashSet<>();
         for (int i = 0; i < veranstaltungsanzahl; i++) {
@@ -102,23 +102,57 @@ public class ModulerstellungController {
                     + "_" + "haeufigkeit");
             String sprache = veranstaltungsFormular.get("veranstaltung" + finalI
                     + "_" + "sprache");
+            Set<Veranstaltungsform> veranstaltungsformen = new HashSet<>();
+            for (int j = 0; j < 5; j++) {
+                int finalJ = j + 1;
+                Map<String, String> veranstaltungsformenFormular = allParams.entrySet().stream()
+                    .filter(e -> e.getKey().startsWith("veranstaltung" + finalI
+                            + "_" + "veranstaltungsform" + finalJ + "_"))
+                    .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+                String checked = veranstaltungsformenFormular.get("veranstaltung" + finalI
+                        + "_" + "veranstaltungsform" + finalJ + "_checked");
+                if (checked != null) {
+                    String form = veranstaltungsformenFormular.get("veranstaltung" + finalI
+                            + "_" + "veranstaltungsform" + finalJ + "_form");
+                    String sws = veranstaltungsformenFormular.get("veranstaltung" + finalI
+                            + "_" + "veranstaltungsform" + finalJ + "_sws");
+                    Veranstaltungsform veranstaltungsform = new Veranstaltungsform();
+                    veranstaltungsform.setForm(form);
+                    if (sws != null) {
+                        if (!sws.equals("")) {
+                            veranstaltungsform.setSemesterWochenStunden(Integer.parseInt(sws));
+                        }
+                    }
+                    veranstaltungsformen.add(veranstaltungsform);
+                }
+            }
+            String veranstaltungsformTextareaChecked = veranstaltungsFormular.get("veranstaltung"
+                    + finalI + "_" + "veranstaltungsform_textarea_checked");
+            if (veranstaltungsformTextareaChecked != null) {
+                String veranstaltungsformTextareaInhalt = veranstaltungsFormular.get("veranstaltung"
+                        + finalI + "_" + "veranstaltungsform_textarea_inhalt");
+                Veranstaltungsform veranstaltungsform = new Veranstaltungsform();
+                veranstaltungsform.setForm(veranstaltungsformTextareaInhalt);
+                veranstaltungsformen.add(veranstaltungsform);
+            }
             Veranstaltung veranstaltung = new Veranstaltung();
             veranstaltung.setTitel(titel);
             veranstaltung.setLeistungspunkte(leistungspunkte);
             Set<Zusatzfeld> zusatzfelder = new HashSet<>();
-            if (!zusatzfeld1Titel.equals("") || !zusatzfeld1Inhalt.equals("")) {
+            if (!zusatzfeld1Titel.equals("") && !zusatzfeld1Inhalt.equals("")) {
                 Zusatzfeld zusatzfeld = new Zusatzfeld();
                 zusatzfeld.setTitel(zusatzfeld1Titel);
                 zusatzfeld.setInhalt(zusatzfeld1Inhalt);
                 zusatzfelder.add(zusatzfeld);
             }
-            if (!zusatzfeld2Titel.equals("") || !zusatzfeld2Inhalt.equals("")) {
+            if (!zusatzfeld2Titel.equals("") && !zusatzfeld2Inhalt.equals("")) {
                 Zusatzfeld zusatzfeld = new Zusatzfeld();
                 zusatzfeld.setTitel(zusatzfeld2Titel);
                 zusatzfeld.setInhalt(zusatzfeld2Inhalt);
                 zusatzfelder.add(zusatzfeld);
             }
             veranstaltung.setZusatzfelder(zusatzfelder);
+            veranstaltung.setVeranstaltungsformen(veranstaltungsformen);
             Veranstaltungsbeschreibung beschreibung = new Veranstaltungsbeschreibung();
             beschreibung.setInhalte(inhalte);
             beschreibung.setLernergebnisse(lernergebnisse);
@@ -131,6 +165,7 @@ public class ModulerstellungController {
             veranstaltungen.add(veranstaltung);
         }
 
+        //Erstellen eines entsprechenden Modul-Objekts
         Modul modul = new Modul();
         modul.setTitelDeutsch(titelDeutsch);
         modul.setTitelEnglisch(titelEnglisch);
@@ -139,8 +174,6 @@ public class ModulerstellungController {
         modul.setModulbeauftragte(modulbeauftragte);
         modul.setModulkategorie(modulkategorie);
         modul.setVeranstaltungen(veranstaltungen);
-
-//            Set<Zusatzfeld> zusatzfelder;
 
         antragService.addModulCreationAntrag(modul,
                 ((KeycloakPrincipal)token.getPrincipal()).getName());
