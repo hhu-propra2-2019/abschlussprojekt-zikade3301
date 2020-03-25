82761e68c75349fab76bd35be25b6345faaefabb
Zhorrid <xintake@gmx.de>
2020-03-25 18:59:14 +0100

    REFACTOR: Copied changed entities from OrgaView branch

--- src/main/java/mops/module/database/Veranstaltungsbeschreibung.java
+++ src/main/java/mops/module/database/Veranstaltungsbeschreibung.java
@@ -1,25 +1,12 @@
 package mops.module.database;
 
-import java.util.HashSet;
-import java.util.Set;
 import javax.persistence.Column;
-import javax.persistence.ElementCollection;
 import javax.persistence.Embeddable;
-import javax.persistence.FetchType;
 import lombok.Data;
 
 @Embeddable
 @Data
 public class Veranstaltungsbeschreibung {
-
-    /**
-     * Konstruktor stellt sicher, dass die Sets nicht null sind.
-     */
-    public Veranstaltungsbeschreibung() {
-        literatur = new HashSet<>();
-        verwendbarkeit = new HashSet<>();
-        voraussetzungenBestehen = new HashSet<>();
-    }
 
     @Column(length = 10000)
     private String inhalte;
@@ -27,17 +14,14 @@ public class Veranstaltungsbeschreibung {
     @Column(length = 10000)
     private String lernergebnisse;
 
-    @ElementCollection(fetch = FetchType.EAGER)
     @Column(length = 10000)
-    private Set<String> literatur;
+    private String literatur;
 
-    @ElementCollection(fetch = FetchType.EAGER)
     @Column(length = 10000)
-    private Set<String> verwendbarkeit;
+    private String verwendbarkeit;
 
-    @ElementCollection(fetch = FetchType.EAGER)
     @Column(length = 10000)
-    private Set<String> voraussetzungenBestehen;
+    private String voraussetzungenBestehen;
 
     private String haeufigkeit;
 
