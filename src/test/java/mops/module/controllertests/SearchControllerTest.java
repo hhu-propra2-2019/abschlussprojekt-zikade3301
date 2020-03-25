72def7882ff715548df2bec73e4edbcce632c12c
MichaMotzi207 <56838501+MichaMotzi207@users.noreply.github.com>
2020-03-25 15:51:12 +0100

    Update SearchControllerTest.java

--- src/test/java/mops/module/controllertests/SearchControllerTest.java
+++ src/test/java/mops/module/controllertests/SearchControllerTest.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @AutoConfigureMockMvc
