57f6c8089243b331e4ef875544c6fcd575731bdd
abot64 <ann.lenden@hhu.de>
2020-03-26 13:12:26 +0100

    Merge branch 'master' of /home/ac/projects/propra2/abschlussprojekt-zikade3301 with conflicts.

---
+++ src/main/java/mops/module/controller/SearchController.java
@@ -0,0 +1,50 @@
+package mops.module.controller;
+
+import static mops.module.keycloak.KeycloakMopsAccount.createAccountFromPrincipal;
+
+import java.util.List;
+import mops.module.database.Modul;
+import mops.module.services.SuchService;
+import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.context.annotation.SessionScope;
+
+@Controller
+@SessionScope
+@RequestMapping("/module")
+public class SearchController {
+
+    private SuchService suchService;
+
+    @Autowired
+    public SearchController(SuchService suchService) {
+        this.suchService = suchService;
+    }
+
+    /**
+     * Searchresults string.
+     *
+     * @param searchField Benutzer Eingabe für das Such Formular
+     * @param token       Token von Keycloak der den Zugriff auf eine Seite regelt
+     * @param model       Model von Keycloak das den Zugriff auf eine Seite regelt
+     * @return            Gibt den String "searchresuls" als Ergebnis der Suche zurück.
+     */
+    @GetMapping("/search")
+    public String search(
+            @RequestParam String searchField,
+            KeycloakAuthenticationToken token,
+            Model model) {
+        if (token != null) {
+            model.addAttribute("account", createAccountFromPrincipal(token));
+        }
+
+        List<Modul> searchResults = suchService.search(searchField);
+        model.addAttribute("searchResults", searchResults);
+        return "searchresults";
+    }
+}
