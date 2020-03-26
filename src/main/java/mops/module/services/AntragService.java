2ee8dd962653357f02603c3519e8d0f20755bd99
Iven Buchholz <Ivenbu@googlemail.com>
2020-03-26 13:41:22 +0100

    REFACTOR: deleted a print.out

--- src/main/java/mops/module/services/AntragService.java
+++ src/main/java/mops/module/services/AntragService.java
@@ -121,8 +121,6 @@ public class AntragService {
     }
 
     public List<Antrag> getAlleAntraegeGeordnetDatum() {
-        System.out.println(antragRepository.findAll());
-
         return getAlleAntraege().stream().sorted(Comparator.comparing(Antrag::getDatumErstellung))
                 .collect(Collectors.toList());
     }
