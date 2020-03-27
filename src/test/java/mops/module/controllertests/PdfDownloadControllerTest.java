3f8d581d764fbaa9b9ba844bdd7ddb1001ecdd04
abot64 <34477414+abot64@users.noreply.github.com>
2020-03-27 15:39:49 +0100

    REFACTOR: Add "dev"-profile annotation to test

--- src/test/java/mops/module/controllertests/PdfDownloadControllerTest.java
+++ src/test/java/mops/module/controllertests/PdfDownloadControllerTest.java
@@ -18,6 +18,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -25,6 +26,7 @@ import org.springframework.web.context.WebApplicationContext;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("dev")
 public class PdfDownloadControllerTest {
 
     @Autowired
