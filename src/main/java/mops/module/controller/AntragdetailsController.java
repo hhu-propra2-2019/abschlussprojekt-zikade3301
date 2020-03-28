9b23b1412360cc2da5c822d54939e237d6cc551f
Iven Buchholz <Ivenbu@googlemail.com>
2020-03-28 15:31:20 +0100

    REFACTOR:outsourced a method

--- src/main/java/mops/module/controller/AntragdetailsController.java
+++ src/main/java/mops/module/controller/AntragdetailsController.java
@@ -78,17 +78,7 @@ public class AntragdetailsController {
         Antrag antrag =  antragService.getAntragById(id);
         Modul modulAlt = modulService.getModulById(antrag.getModulId());
         Modul modulNeu = new Modul();
-
-        //ModulAlt kopieren
-        //TODO - eventuell auslagern
-        for (Field field : modulAlt.getClass().getDeclaredFields()) {
-            field.setAccessible(true);
-            try {
-                field.set(modulNeu, field.get(modulAlt));
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
-            }
-        }
+        ModulService.kopiereModul(modulAlt, modulNeu);
 
         ModulService.applyAntragOnModul(modulNeu,antrag);
 
