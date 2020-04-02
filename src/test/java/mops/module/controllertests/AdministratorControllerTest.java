34f86e07961d8d6b226e8d54e8ec41b55ed30465
MichaMotzi207 <michael.motzko@gmx.net>
2020-04-02 20:49:41 +0200

    Add: ControllerTest for deleting an Antrag

--- src/test/java/mops/module/controllertests/AdministratorControllerTest.java
+++ src/test/java/mops/module/controllertests/AdministratorControllerTest.java
@@ -2,17 +2,23 @@ package mops.module.controllertests;
 
 import static mops.module.controllertests.AuthenticationTokenGenerator.generateAuthenticationToken;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
+
+import mops.module.database.Antrag;
+import mops.module.services.AntragService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
@@ -29,12 +35,18 @@ class AdministratorControllerTest {
     private MockMvc mvc;
 
 
+    @MockBean
+    private AntragService antragService;
+
     @BeforeEach
     void setUp() {
         mvc = MockMvcBuilders.webAppContextSetup(context)
                 .alwaysDo(print())
                 .apply(springSecurity())
                 .build();
+
+        Antrag antragToDelte = new Antrag();
+        when(antragService.getAntragById(3302L)).thenReturn(antragToDelte);
     }
 
     private final String expect = "administrator";
@@ -89,5 +101,78 @@ class AdministratorControllerTest {
                 () -> {
                     mvc.perform(get("/module/administrator")).andExpect(view().name(expect));
                 });
+    }
+
+    @Test
+    void testDeleteAntragViewName() throws Exception {
+        SecurityContextHolder
+                .getContext()
+                .setAuthentication(generateAuthenticationToken("sekretariat"));
+
+        mvc.perform(post("/module/deleteAntrag")
+                .param("antragID", "3302"))
+                .andExpect(view().name(expect));
+    }
+
+    @Test
+    void testDeleteAntragAccessForAdmin() throws Exception {
+        SecurityContextHolder
+                .getContext()
+                .setAuthentication(generateAuthenticationToken("sekretariat"));
+
+        mvc.perform(post("/module/deleteAntrag")
+                .param("antragID", "3302"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void testDeleteAntragNoAccessIfNotLoggedIn() {
+        assertThrows(java.lang.AssertionError.class,
+                () -> {
+                    mvc.perform(post("/module/deleteAntrag")
+                            .param("antragID", "3302"))
+                            .andExpect(view().name(expect));
+                });
+    }
+
+    @Test
+    void testDeleteAntragNoAccessForStudents() {
+        SecurityContextHolder
+                .getContext()
+                .setAuthentication(generateAuthenticationToken("studentin"));
+
+        assertThrows(java.lang.AssertionError.class,
+                () -> {
+                    mvc.perform(post("/module/deleteAntrag")
+                            .param("antragID", "3302"))
+                            .andExpect(view().name(expect));
+                });
+    }
+
+    @Test
+    void testDeleteAntragNoAccessForOrganisator() {
+        SecurityContextHolder
+                .getContext()
+                .setAuthentication(generateAuthenticationToken("orga"));
+
+        assertThrows(java.lang.AssertionError.class,
+                () -> {
+                    mvc.perform(post("/module/deleteAntrag")
+                            .param("antragID", "3302"))
+                            .andExpect(view().name(expect));
+                });
+    }
+
+    @Test
+    void changeModulVisibilityControllerCallsChangeVisibility() throws Exception {
+        SecurityContextHolder
+                .getContext()
+                .setAuthentication(generateAuthenticationToken("sekretariat"));
+
+        mvc.perform(post("/module/deleteAntrag")
+                .param("antragID", "3302"));
+
+        verify(antragService)
+                .deleteAntrag(3302L);
     }
 }
